24910b6bc1552cf3a9988d0840291e8bc3868872
vincenterc <chung.menghsun@gmail.com>
2019-03-04 18:54:39 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import PageWrapper from '../components/page-wrapper'
 
-export default PageWrapper(() => <div>Gatsby starter!</div>)
+export default PageWrapper(() => <div>SKE48XdTV 10th</div>)
