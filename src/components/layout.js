349e5c0709e66005d57bbb3d050c3d15c57c5e1f
vincenterr <chung.menghsun@gmail.com>
2019-03-05 00:05:15 +0800

    Add title, description meta and favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Helmet } from 'react-helmet'
+
 import './layout.css'
+import favicon from '../../assets/img/favicon.ico'
 
 class Layout extends React.Component {
   render() {
     let { children } = this.props
 
-    return <Wrapper>{children}</Wrapper>
+    return (
+      <Wrapper>
+        <Helmet>
+          <title>SKE48XdTV 10th</title>
+          <meta name="description" content="Links of SKE48XdTV 10th" />
+          <link rel="icon" href={favicon} />
+        </Helmet>
+
+        {children}
+      </Wrapper>
+    )
   }
 }
 
